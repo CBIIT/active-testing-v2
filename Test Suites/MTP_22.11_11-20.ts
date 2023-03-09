633cd5cedb46e41b8b8061832ddc5e96e93c4b69
laxmi.lolla@gmail.com <laxmi.lolla@gmail.com>
2023-03-09 09:09:28 -0500

    11-20

--- Test Suites/MTP_22.11_11-20.ts
+++ Test Suites/MTP_22.11_11-20.ts
@@ -1,7 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <TestSuiteEntity>
    <description></description>
-   <name>MTP_22.11_11-30</name>
+   <name>MTP_22.11_11-20</name>
    <tag></tag>
    <isRerun>false</isRerun>
    <mailRecipient></mailRecipient>
