23811a162225ee22a052a3c589f9eb3755deb39c
laxmi.lolla@gmail.com <laxmi.lolla@gmail.com>
2023-06-21 11:52:39 -0400

    MTP_API_pilot

---
+++ Test Suites/MTPAPI.ts
@@ -0,0 +1,39 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>MTPAPI</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>ebf0638f-3aed-4b22-bf37-fe7c1d830e8b</testSuiteGuid>
+   <testCaseLink>
+      <guid>8f07e42a-be64-4d54-9a57-30c63e61f90d</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/MTP_1</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>5a7cbf5a-1e69-4a29-b222-21953e3a3235</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>6fa83114-a3a5-46ed-a559-e76540b9b6eb</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>f9370acd-a6c8-455c-bd43-b5099f227e6e</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
