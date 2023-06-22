3899edd47b6687ed62d7dc5d265c436b010152d9
laxmi.lolla@gmail.com <laxmi.lolla@gmail.com>
2023-06-22 07:51:56 -0400

    MTP Split running chop1

--- Test Suites/MTPAPI_chop1.ts
+++ Test Suites/MTPAPI_chop1.ts
@@ -1,7 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <TestSuiteEntity>
    <description></description>
-   <name>MTPAPI</name>
+   <name>MTPAPI_chop1</name>
    <tag></tag>
    <isRerun>false</isRerun>
    <mailRecipient></mailRecipient>
--- Test Suites/MTP_collection.ts
+++ Test Suites/MTP_collection.ts
@@ -15,7 +15,7 @@
             <runConfigurationId>Chrome</runConfigurationId>
          </configuration>
          <runEnabled>true</runEnabled>
-         <testSuiteEntity>Test Suites/MTPAPI</testSuiteEntity>
+         <testSuiteEntity>Test Suites/MTPAPI_chop1</testSuiteEntity>
       </TestSuiteRunConfiguration>
    </testSuiteRunConfigurations>
 </TestSuiteCollectionEntity>
