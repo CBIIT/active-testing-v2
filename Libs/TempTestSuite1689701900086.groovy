e13e3a89d043db4f2b7acfc6c347cb3c0b1759c0
laxmilolla <lollal@mail.nih.gov>
2023-07-18 13:40:11 -0400

    plotLy

---
+++ Libs/TempTestSuite1689701900086.groovy
@@ -0,0 +1,36 @@
+import com.kms.katalon.core.logging.KeywordLogger
+import com.kms.katalon.core.exception.StepFailedException
+import com.kms.katalon.core.reporting.ReportUtil
+import com.kms.katalon.core.main.TestCaseMain
+import com.kms.katalon.core.testdata.TestDataColumn
+import com.kms.katalon.core.testcase.TestCaseBinding
+import com.kms.katalon.core.driver.internal.DriverCleanerCollector
+import com.kms.katalon.core.model.FailureHandling
+import com.kms.katalon.core.configuration.RunConfiguration
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import internal.GlobalVariable as GlobalVariable
+
+Map<String, String> suiteProperties = new HashMap<String, String>();
+
+suiteProperties.put('id', 'Test Suites/MTPAPI_Plotly')
+suiteProperties.put('name', 'MTPAPI_Plotly')
+suiteProperties.put('description', '')
+suiteProperties.put('rerunTestFailImmediately', 'true')
+suiteProperties.put('retryCount', '3')
+ 
+
+DriverCleanerCollector.getInstance().addDriverCleaner(new com.kms.katalon.core.webui.contribution.WebUiDriverCleaner())
+DriverCleanerCollector.getInstance().addDriverCleaner(new com.kms.katalon.core.mobile.contribution.MobileDriverCleaner())
+DriverCleanerCollector.getInstance().addDriverCleaner(new com.kms.katalon.core.cucumber.keyword.internal.CucumberDriverCleaner())
+DriverCleanerCollector.getInstance().addDriverCleaner(new com.kms.katalon.core.windows.keyword.contribution.WindowsDriverCleaner())
+DriverCleanerCollector.getInstance().addDriverCleaner(new com.kms.katalon.core.testng.keyword.internal.TestNGDriverCleaner())
+
+
+
+RunConfiguration.setExecutionSettingFile("C:\\Users\\lollal\\Documents\\Katalon\\V2_Visual_test\\active-testing-v2\\Reports\\20230718_133820\\MTPAPI_Plotly\\20230718_133820\\execution.properties")
+
+TestCaseMain.beforeStart()
+
+TestCaseMain.startTestSuite('Test Suites/MTPAPI_Plotly', suiteProperties, new File("C:\\Users\\lollal\\Documents\\Katalon\\V2_Visual_test\\active-testing-v2\\Reports\\20230718_133820\\MTPAPI_Plotly\\20230718_133820\\testCaseBinding"))
